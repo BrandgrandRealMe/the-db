d2ab4fb4c2bb340c950babf58eb70275dc154ece
BrandgrandRealMe <brandonthedev@proton.me>
2025-06-03 15:37:55 -0500

    FIX

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com//BrandgrandRealMe/the-db/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/BrandgrandRealMe/the-db/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
